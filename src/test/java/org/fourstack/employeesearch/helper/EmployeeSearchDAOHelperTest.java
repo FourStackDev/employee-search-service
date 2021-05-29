700b44c7cb58b6ba5fc6093d002de5461a2d1293
ManjunathMGowda92 <manjunathcs052@gmail.com>
2021-05-29 13:51:54 +0530

    added new test case

--- src/test/java/org/fourstack/employeesearch/helper/EmployeeSearchDAOHelperTest.java
+++ src/test/java/org/fourstack/employeesearch/helper/EmployeeSearchDAOHelperTest.java
@@ -1,7 +1,9 @@
 package org.fourstack.employeesearch.helper;
 
+import java.util.List;
 import java.util.Optional;
 
+import org.fourstack.employeesearch.constants.EmployeeSearchConstants;
 import org.fourstack.employeesearch.dao.EmployeeRepository;
 import org.fourstack.employeesearch.models.Employee;
 import org.fourstack.employeesearch.utils.CommonUtils;
@@ -12,6 +14,11 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -46,5 +53,32 @@ public class EmployeeSearchDAOHelperTest {
 		// Assert the target method with Not Null Object
 		Assertions.assertNotNull(daoHelper.getEmployeeById(Mockito.anyString()));
 		log.info("EmployeeSearchDAOHelperTest: test_getEmployeeById() Method - End");
+	}
+	
+	@Test
+	public void test_getEmployeePage() {
+		log.info("EmployeeSearchDAOHelperTest: test_getEmployeePage() Method - Start");
+		
+		// Create a Page Object 
+		Pageable page = getPageObject();
+		
+		// Create Employee Array Objects
+		Employee[] empArray = CommonUtils.deserializeJsonStringToResource(CommonUtils.getFileContentUsingResourceStream("json-files/emp-list-data.json", this.getClass().getClassLoader()), Employee[].class);
+		
+		// Convert Employee Array to List
+		List<Employee> empList = List.of(empArray);
+		
+		// Create Employee Page
+		Page<Employee> empPage = new PageImpl<>(empList);
+		
+		// Mock the Methods
+		Mockito.when(empRepository.findAll(page)).thenReturn(empPage);
+		Assertions.assertNotNull(daoHelper.getEmployeePage(page));
+		
+		log.info("EmployeeSearchDAOHelperTest: test_getEmployeePage() Method - End");
+	}
+	
+	private PageRequest getPageObject() {
+		return PageRequest.of(0, 5, Sort.by(Sort.Direction.ASC, EmployeeSearchConstants.EMPLOYEE_ID));
 	}
 }
